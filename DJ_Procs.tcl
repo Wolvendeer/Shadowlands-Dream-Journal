41f54a76cf1572e4a2140525813df34ae4487e50
unknown <Wolvendeer@.(none)>
2011-03-19 17:40:39 -0400

    Made the filltree command to fill the .dr tree.  Will edit later as other tabs are filled out.

--- DJ_Procs.tcl
+++ DJ_Procs.tcl
@@ -7,6 +7,34 @@ proc pruc {name argl body} {
     proc $name $argl $prefix$body
 }
 
-pruc filltree {&tree &db} {
+pruc filltree {&tree &vwcurse} {
+	mk::loop vwcurse {
+		set Year [mk::get vwcurse Year]
+		set Month [mk::get vwcurse Month]
+		set Day [mk::get vwcurse Day]
+		
+		if {$tree exists .$Year} {
+			if {$tree exists .$Year.$Month} {
+				if {$tree exists .$Year.$Month.$Day} {
+					$tree insert end .$Year.$Month.$Day .$Year.$Month.$Day.#auto -text [mk::get vwcurse Title] -data [mk::cursor position vwcurse]
+				} else {
+					$tree insert end .$Year.$Month .$Year.$Month.$Day -text $Day
+					$tree insert end .$Year.$Month.$Day .$Year.$Month.$Day.#auto -text [mk::get vwcurse Title] -data [mk::cursor position vwcurse]
+				}
+			} else {
+				$tree insert end .$Year .$Year.$Month -text $Month
+				$tree insert end .$Year.$Month .$Year.$Month.$Day -text $Day
+				$tree insert end .$Year.$Month.$Day .$Year.$Month.$Day.#auto -text [mk::get vwcurse Title] -data [mk::cursor position vwcurse]
+			}
+		} else {
+			$tree insert end root .$Year -text $Year
+			$tree insert end .$Year .$Year.$Month -text $Month
+			$tree insert end .$Year.$Month .$Year.$Month.$Day -text $Day
+			$tree insert end .$Year.$Month.$Day .$Year.$Month.$Day.#auto -text [mk::get vwcurse Title] -data [mk::cursor position vwcurse]
+		}
+	}
+}
+
+proc drsave {} {
 	
 }
